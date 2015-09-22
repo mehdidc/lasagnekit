bfc39ffd1ff3b31203719b148650a94521513c1a
mehdi cherti <mehdi@cherti.name>
2015-09-22 19:37:28 +0200

    add dtd texture dataset

---
+++ lasagnekit/datasets/dtd.py
@@ -0,0 +1,35 @@
+import lasagnekit
+import os
+import re
+from glob import glob
+from skimage.io import imread
+from skimage.transform import resize
+import numpy as np
+
+class DTD(object):
+    category_names = [
+        re.match(os.getenv("DATA_PATH") + "/textures-dtd/images/(.*)", dirname).groups(1)[0]
+        for dirname in glob(os.getenv("DATA_PATH") + "/textures-dtd/images/*")
+    ]
+    all_images = list(glob(os.getenv("DATA_PATH") + "/textures-dtd/images/**/*.jpg"))
+
+    def __init__(self, filter_by=lambda x:x[0:100], 
+                       w=200, h=200):
+        self.filter_by = filter_by
+        self.w = w
+        self.h = h
+
+    def load(self):
+        X = []
+        y = []
+        images = self.filter_by(self.all_images)
+        for image in images:
+            category_name = re.match(os.getenv("DATA_PATH") + "/textures-dtd/images/(.*)/.*jpg", image).groups(1)[0]
+            image = imread(image)
+            image = resize(image, (self.h, self.w), preserve_range=True)
+            X.append(image)
+            y.append(category_name)
+        X = np.array(X).astype(np.float32)
+        y = np.array(y)
+        self.X = X
+        self.y = y
