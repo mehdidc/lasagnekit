9ba7553139548eb710d3e59b0862b79dc3310c8e
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-11-13 23:21:42 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -9,4 +9,20 @@ setup(
     license="BSD",
     keywords="",
     url="",
+    zip_safe=False,  # the package can run out of an .egg file
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS'],
+    platforms='any',
+    packages=['lasagnekit', 'lasagnekit.misc',
+              'lasagnekit.generative',
+              'lasagnekit.datasets',
+              ]
 )
